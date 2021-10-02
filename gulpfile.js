36bfe8acbd1b336145558fe39b3df7ec02d3f1b5
janhavi-dave <91345503+janhavi-dave@users.noreply.github.com>
2021-10-02 17:46:39 +0530

    update

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
-function defaultTask(cb) {
-  // place code for your default task here
-  cb();
-}
+const gulp = require('gulp');
+const themekit = require('@shopify/themekit');
+const sass= require('gulp-sass');
 
-exports.default = defaultTask     
+gulp.task('sass',function(){
+    return gulp.src('scss/global.scss')
+})
